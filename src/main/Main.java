6e4b56032f4392aa38532735586d02a22054b76a
Egor1705 <49376739+Egor1705@users.noreply.github.com>
2019-11-04 23:45:02 +0300

    commit 2

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,7 +13,6 @@ public class Main {
 
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
-
 		Scanner sc = new Scanner(System.in);
 
 		String driver = "com.mysql.cj.jdbc.Driver";
@@ -22,17 +21,18 @@ public class Main {
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} // необязательно
+		}
 		String url = "jdbc:mysql://localhost/Bank?useUnicode=" + "true&useJDBCCompliantTimezoneShift="
 				+ "true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 		Connection con = DriverManager.getConnection(url, "root", "Bottomass1375");
 		Statement st = con.createStatement();
 
 		boolean check = true;
+		
 		while (check) {
 			System.out.println("What are you going to do?\n" + "1)Open the bill\n" + "2)Close the bill\n"
 					+ "3)Put money on the bill\n" + "4)Take money from the bill\n" + "5)Show the sum\n"
-					+ "6)Finish\n ");
+					+ "0)Finish\n ");
 			String temp = sc.nextLine();
 
 			switch (temp) {
@@ -40,91 +40,57 @@ public class Main {
 				System.out.println("Input the number of bill :\n ");
 				int bill = sc.nextInt();
 
-				String sql = "Select exists(select id_bill from Bill where id_bill='" + bill + "');";
-
-				ResultSet resultSet1 = st.executeQuery(sql);
-				String name1 = "";
-				while (resultSet1.next()) {
-					name1 = resultSet1.getString(1);
-					System.out.println("------ " + name1);
-				}
-				resultSet1.close();
-
-				if (!name1.equals("1")) {
-					System.out.println("Сначало создайте счет \n");
-					System.out.println("Введите Имя и Фамилию владельца \n");
-					Transaction.createBill(bill, sc.nextLine(), sc.nextLine());
-				} else {
-					Transaction.openBill(bill);
-				}
+				Transaction.openBill(bill);
 
 				break;
 			}
-			// case "2": {
-			// System.out.println("Input the number of bill :\n ");
-			//
-			// String sql2 = "update Bill SET STATUS = Close";
-			// break;
-			// }
-			//
-			// case "3": {
-			// // if()
-			// String sql3 = "insert into transaction values('" + 0 + "', '" + sc.nextInt()
-			// + "')";
-			// break;
-			// }
-			//
-			// case "4": {
-			//
-			// break;
-			// }
-			//
-			// case "5": {
-			//
-			// break;
-			// }
+			case "2": {
+				System.out.println("Input the number of bill :\n ");
+				int bill = sc.nextInt();
+Transaction.closeBill(bill);
+				break;
+			}
+
+			 case "3": {
+				 System.out.println("Input the number of bill :\n ");
+					int bill = sc.nextInt();
+				 System.out.println("Input sum which you want to put :\n");
+				int sum = sc.nextInt();
+			 Transaction.putMoney(bill, sum);
+			 break;
+			 }
+			
+			 case "4": {
+			
+				 System.out.println("Input the number of bill :\n ");
+					int bill = sc.nextInt();
+				 System.out.println("Input sum which you want to take :\n");
+				int sum = sc.nextInt();
+			 Transaction.takeMoney(bill, sum);
+				 
+				 
+			 break;
+			 }
+			
+			 case "5": {
+				 System.out.println("Input the number of bill :\n ");
+				int bill = sc.nextInt();
+				 Transaction.showBill(bill);
+				 
+				 
+			 break;
+			 }
 
 			default: {
-				check = false;
 				System.out.println("exit");
+				check = false;
+				
 			}
 			}
 
 		}
 
-		//
-		// int id = 1;
-		// int sum1 = 200;
-		//
-		//
-		// String sqlInsert = "insert into transaction values('" +id+ "', '" +sum+ "')";
-		//
-		//
-		// int x = statement.executeUpdate(sqlInsert);
-		//
-		// if (x > 0)
-		// {
-		// System.out.println("Successfully Inserted");
-		// }
-		// else
-		// {
-		// System.out.println("Insert Failed");
-		// }
-
-		//
-		// String sqlInsert = "insert into bill values('" +bill_id+ "', '" +name+
-		// "', '" +surName+ "', '" +status+ "')";
-		//
-		// int x = statement.executeUpdate(sqlInsert);
-		//
-		// if (x > 0)
-		// {
-		// System.out.println("Successfully Inserted");
-		// }
-		// else
-		// {
-		// System.out.println("Insert Failed");
-		// }
+		
 
 		String sqlList = "SELECT * FROM BILL";
 
@@ -142,44 +108,29 @@ public class Main {
 		}
 		//
 		resultSet.close();
-		//
-		//
-		// String exists = "if exists(select * from BILL \r\n" +
-		// "where Name = 'Vasya' and BILL_ID = 1)\r\n" +
-		// "begin\r\n" +
-		// " PRINT 'Column Exists in given table'\r\n" +
-		// "end\r\n" +
-		// "else\r\n" +
-		// " PRINT 'Column does not Exist in given table'";
-		//
-		// ResultSet resultSet1 = statement.executeQuery(sqlList);
-		//
-		//
-		// while (resultSet1.next()) {
-		// String name1 = resultSet1.getString(1);
-		// System.out.println("------ "+name1);
-		//
-		// }
+		System.out.println("\n");
+		
 
-		// String sqlList1 = "SELECT * FROM TRANSACTION";
+		 String sqlList1 = "SELECT * FROM TRANSACTION";
+		
+		 ResultSet resultSet1 = st.executeQuery(sqlList1);
+		
+		
+		
+		
+		 while (resultSet1.next()) {
+		 int id = resultSet1.getInt("id");
+		
+		int sum1 = resultSet1.getInt("sum");
+		int id_bill = resultSet1.getInt("id_transact_bill");
 		//
-		// ResultSet resultSet1 = statement.executeQuery(sqlList);
 		//
-		//
-		//
-		//
-		// while (resultSet1.next()) {
-		// id = resultSet1.getInt("ID");
-		//
-		// sum = resultSet1.getInt("SUM");
-		//
-		//
-		// System.out.println(" " + resultSet1.getInt("ID") + ",\t "
-		// + resultSet1.getInt("SUM"));
-		//
-		// }
-		//
-		// resultSet1.close();
+		 System.out.println(" " + resultSet1.getInt("id") + ",\t "
+		 + resultSet1.getInt("sum")+",\t " +resultSet1.getInt("id_transact_bill") + ",\t " );
+		
+		 }
+		
+		 resultSet1.close();
 
 	}
 
