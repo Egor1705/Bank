e0999fbb176909cb424abd9afd27b51c136e5edd
Egor1705 <49376739+Egor1705@users.noreply.github.com>
2019-11-10 12:36:21 +0300

    commit2

--- src/impl/Transaction.java
+++ src/impl/Transaction.java
@@ -1,5 +1,3 @@
-package impl;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -9,12 +7,11 @@ import java.util.Scanner;
 
 public class Transaction {
 
-	private final static String URL = "jdbc:mysql://localhost/Bank?useUnicode=" + "true&useJDBCCompliantTimezoneShift="
-			+ "true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	private final static String URL = "jdbc:db2://172.20.2.116:5035/DALLASB";
 
-	private final static String DRIVER = "com.mysql.cj.jdbc.Driver";
-	private final static String root = "root";
-	private final static String password = "Bottomass1375";
+	private final static String DRIVER = "com.ibm.db2.jcc.DB2Driver";
+	private final static String root = "USER06A";
+	private final static String password = "USER06A";
 
 	private static Scanner sc = new Scanner(System.in);
 
@@ -25,34 +22,36 @@ public class Transaction {
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} // необязательно
+		}
 
 		Connection con = DriverManager.getConnection(URL, root, password);
 		Statement st = con.createStatement();
 
 		// String sql = "update Bill set status_of_bill = 'Open'";
 
-		String sql = "Select exists(select id_bill from Bill where id_bill='" + id_Bill + "');";
+		// String sql = "Select exists(select BILL_ID FROM BILL where BILL_ID='" +
+		// id_Bill + "');";
 
+		String sql = "select bill_id from bill where bill_id='" + id_Bill + "';";
 		ResultSet resultSet1 = st.executeQuery(sql);
 		String name1 = "";
 		while (resultSet1.next()) {
 			name1 = resultSet1.getString(1);
-			System.out.println("------ " + name1);
+			//System.out.println("------ " + name1);
 		}
 		resultSet1.close();
 		int resultSet = 0;
-		if (!name1.equals("1")) {
-			System.out.println("Сначало создайте счет \n");
-			System.out.println("Введите Имя и Фамилию владельца \n");
+		if (!name1.equals(Integer.toString(id_Bill))) {
+			System.out.println("At first you should create a bill \n");
+			System.out.println("Input owner's name and surname \n");
 			String sql1 = "insert into Bill values('" + id_Bill + "', '" + sc.nextLine() + "', '" + sc.nextLine()
 					+ "', '" + "Open" + "');";
 			resultSet = st.executeUpdate(sql1);
 		} else {
 
-			String sql2 = "update  Bill set status_of_bill = 'Open' where id_bill = '" + id_Bill + "' ";
+			String sql2 = "update  Bill set status = 'Open' where bill_id = '" + id_Bill + "' ";
 			resultSet = st.executeUpdate(sql2);
-			System.out.println("Счет открыт ");
+			System.out.println("Bill is open ");
 		}
 		con.close();
 
@@ -70,38 +69,36 @@ public class Transaction {
 		Connection con = DriverManager.getConnection(URL, root, password);
 		Statement st = con.createStatement();
 
-		String sql = "Select exists(select id_bill from Bill where id_bill='" + id_Bill + "');";
+		String sql = "select bill_id from bill where bill_id='" + id_Bill + "';";
 
 		ResultSet resultSet1 = st.executeQuery(sql);
 		String name1 = "";
 		while (resultSet1.next()) {
 			name1 = resultSet1.getString(1);
-			System.out.println("------ " + name1);
+		//	System.out.println("------ " + name1);
 		}
 		resultSet1.close();
 		int resultSet = 0;
-		
-		String sql1 = "select status_of_bill from bill where id_bill='"+id_Bill+ "';";
+
+		String sql1 = "select status from bill where bill_id='" + id_Bill + "';";
 		ResultSet resultSetStatus = st.executeQuery(sql1);
 		String name2 = "";
 		while (resultSetStatus.next()) {
 			name2 = resultSetStatus.getString(1);
-			System.out.println("------ " + name2);
+		//	System.out.println("------ " + name2);
 		}
-		resultSetStatus.close();	
-		
-		if (!name1.equals("1")) {
-			System.out.println("Такого счета не существует \n");
+		resultSetStatus.close();
+
+		if (!name1.equals(Integer.toString(id_Bill))) {
+			System.out.println("such  bill does not exist \n");
 		}
-	
-		
-		else if(name2.equals("Close")) {
-			System.out.println("Этот счет уже закрыт \n");
-		}
-		else {
-			String sql2 = "update  Bill set status_of_bill = 'Close' where id_bill = '" + id_Bill + "' ";
+
+		else if (name2.equals("Close")) {
+			System.out.println("This bill is already closed \n");
+		} else {
+			String sql2 = "update  Bill set status = 'Close' where bill_id = '" + id_Bill + "' ";
 			resultSet = st.executeUpdate(sql2);
-			System.out.println("Счет закрыт \n");
+			System.out.println("The Bill is closed \n");
 		}
 
 		con.close();
@@ -119,47 +116,52 @@ public class Transaction {
 
 		Connection con = DriverManager.getConnection(URL, root, password);
 		Statement st = con.createStatement();
-		String sql = "Select exists(select id_bill from Bill where id_bill='" + id_Bill + "');";
+		String sql = "select bill_id from bill where bill_id='" + id_Bill + "';";
 
 		ResultSet resultSet1 = st.executeQuery(sql);
 		String name1 = "";
 		while (resultSet1.next()) {
 			name1 = resultSet1.getString(1);
-			System.out.println("------ " + name1);
+		//	System.out.println("------ " + name1);
 		}
 		resultSet1.close();
-		
-		String sql1 = "select status_of_bill from bill where id_bill='"+id_Bill+ "';";
+
+		String sql1 = "select status from bill where bill_id='" + id_Bill + "';";
 		ResultSet resultSetStatus = st.executeQuery(sql1);
 		String name2 = "";
 		while (resultSetStatus.next()) {
 			name2 = resultSetStatus.getString(1);
-			System.out.println("------ " + name2);
+		//	System.out.println("------ " + name2);
 		}
-		resultSet1.close();	
-		
+		resultSet1.close();
+
 		int resultSet = 0;
-		if (!name1.equals("1")) {
-		System.out.println("Такого счета не существует \n");
-		}	
-		else if(name2.equals("Close")) {
-			System.out.println("Любые транзакции невозможны,счет закрыт \n");
+		String sqlCount = "select count(*) from Transaction;";
+		ResultSet rsCount = st.executeQuery(sqlCount);
+		String nameCount = "";
+		while (rsCount.next()) {
+			nameCount = rsCount.getString(1);
 		}
-		
+		int i = 1 + Integer.parseInt(nameCount);
+
+		if (!name1.equals(Integer.toString(id_Bill))) {
+			System.out.println("such  bill does not exist \n");
+		} else if (name2.equals("Close")) {
+			System.out.println("No transactions possible, bill is closed \n");
+		}
+
 		else {
-			String sql2 = "insert into Transaction values('" + 5 + "', '" + sum + "','" +id_Bill + "')";
+			String sql2 = "insert into Transaction values('" + i + "',' " + id_Bill + "', '" + sum + "')";
 			resultSet = st.executeUpdate(sql2);
-			System.out.println("Положено "+sum+ " денег"+ "\n");
+			System.out.println("Put " + sum + " money" + "\n");
 		}
-		
+
 		con.close();
-		
-		
-		
+
 	}
 
 	public static void takeMoney(int id_Bill, int sum) throws SQLException {
-		
+
 		try {
 			Class.forName(DRIVER);
 		} catch (ClassNotFoundException e) {
@@ -169,57 +171,63 @@ public class Transaction {
 
 		Connection con = DriverManager.getConnection(URL, root, password);
 		Statement st = con.createStatement();
-		String sql = "Select exists(select id_bill from Bill where id_bill='" + id_Bill + "');";
+		String sql = "select bill_id from bill where bill_id='" + id_Bill + "';";
 
 		ResultSet resultSet1 = st.executeQuery(sql);
 		String name1 = "";
 		while (resultSet1.next()) {
 			name1 = resultSet1.getString(1);
-			System.out.println("------ " + name1);
+		//	System.out.println("------ " + name1);
 		}
 		resultSet1.close();
-		
-		String sql1 = "select status_of_bill from bill where id_bill='"+id_Bill+ "';";
+
+		String sql1 = "select status from bill where bill_id='" + id_Bill + "';";
 		ResultSet resultSetStatus = st.executeQuery(sql1);
 		String name2 = "";
 		while (resultSetStatus.next()) {
 			name2 = resultSetStatus.getString(1);
-			System.out.println("------ " + name2);
+			//System.out.println("------ " + name2);
 		}
-		resultSet1.close();	
-		
-		
-		
-		
-		String sql2 = "select sum(sum) from transaction \r\n" + 
-				"where id_transact_bill='"+id_Bill+"';";
+		resultSet1.close();
+
+		String sql2 = "select sum(sum) from transaction \r\n" + "where id='" + id_Bill + "';";
 		ResultSet resultSet = st.executeQuery(sql2);
 		String name3 = "";
-		
-		while(resultSet.next()) {
-			 name3 = resultSet.getString(1);
-			System.out.println("sum "+ name3);
+
+		while (resultSet.next()) {
+			name3 = resultSet.getString(1);
+		//	System.out.println("sum " + name3);
 		}
-		
-		
-		if (!name1.equals("1")) {
-		System.out.println("Такого счета не существует \n");
-		}	
-		else if(name2.equals("Close")) {
-			System.out.println("Любые транзакции невозможны,счет закрыт \n");
+		int count = 0;
+		try {
+			count = Integer.parseInt(name3);
+		} catch (NumberFormatException e) {
+
 		}
-		else if(sum>Integer.parseInt(name3)) {
-			System.out.println("На этом счете нету столько денег \n");
+
+		String sqlCount = "select count(*) from Transaction;";
+		ResultSet rsCount = st.executeQuery(sqlCount);
+		String nameCount = "";
+		while (rsCount.next()) {
+			nameCount = rsCount.getString(1);
 		}
-		else {
-			String sql3 = "insert into Transaction values('" + 3 + "', '" + -sum + "','" +id_Bill + "')";
-			int resultSet2 = st.executeUpdate(sql2);
-			System.out.println("Снято "+sum+ " денег"+ "\n");
+
+		int i = 1 + Integer.parseInt(nameCount);
+		if (!name1.equals(Integer.toString(id_Bill))) {
+			System.out.println("such  bill does not exist  \n");
+		} else if (name2.equals("Close")) {
+			System.out.println("No transactions possible, bill is closed \n");
+		} else if (sum > count) {
+			System.out.println("There isn’t so much money in this bill \n");
+		} else {
+			String sql3 = "insert into Transaction values('" + i + "',' " + id_Bill + "', '" + -sum + "')";
+			System.out.println();
+			int resultSet2 = st.executeUpdate(sql3);
+			System.out.println("Take " + sum + " money" + "\n");
 		}
-		
+
 		con.close();
-		
-		
+
 	}
 
 	public static void showBill(int id_Bill) throws SQLException {
@@ -233,42 +241,112 @@ public class Transaction {
 
 		Connection con = DriverManager.getConnection(URL, root, password);
 		Statement st = con.createStatement();
-		String sql = "Select exists(select id_bill from Bill where id_bill='" + id_Bill + "');";
+		String sql = "select bill_id from bill where bill_id='" + id_Bill + "';";
 
 		ResultSet resultSet1 = st.executeQuery(sql);
 		String name1 = "";
 		while (resultSet1.next()) {
 			name1 = resultSet1.getString(1);
-			System.out.println("------ " + name1);
+		//	System.out.println("------ " + name1);
 		}
 		resultSet1.close();
-		
-		String sql1 = "select status_of_bill from bill where id_bill='"+id_Bill+ "';";
+
+		String sql1 = "select status from bill where bill_id='" + id_Bill + "';";
 		ResultSet resultSetStatus = st.executeQuery(sql1);
 		String name2 = "";
 		while (resultSetStatus.next()) {
 			name2 = resultSetStatus.getString(1);
-			System.out.println("------ " + name2);
+		//	System.out.println("------ " + name2);
 		}
-		resultSet1.close();	
-		
+		resultSet1.close();
+
 		ResultSet resultSet = null;
-		if (!name1.equals("1")) {
-		System.out.println("Такого счета не существует \n");
-		}	
-		else if(name2.equals("Close")) {
-			System.out.println("Любые транзакции невозможны,счет закрыт \n");
-		}
-		else {
-			String sql2 = "select sum(sum) from transaction \r\n" + 
-					"where id_transact_bill='"+id_Bill+"';";
+		if (!name1.equals(Integer.toString(id_Bill))) {
+			System.out.println("such  bill does not exist \n");
+		} else if (name2.equals("Close")) {
+			System.out.println("No transactions possible, bill is closed \n");
+		} else {
+			String sql2 = "select sum(sum) from transaction \r\n" + "where id='" + id_Bill + "';";
 			resultSet = st.executeQuery(sql2);
-			while(resultSet.next()) {
+			while (resultSet.next()) {
 				String name3 = resultSet.getString(1);
-				System.out.println("sum "+ name3);
+				System.out.println("sum " + name3);
 			}
 		}
+
+	}
 	
+	
+	
+	
+	public static void showAllBills() throws SQLException {
+		try {
+			Class.forName(DRIVER);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // необязательно
+
+		Connection con = DriverManager.getConnection(URL, root, password);
+		Statement st = con.createStatement();
+		
+		String sqlList = "SELECT * FROM BILL";
+
+		ResultSet resultSet = st.executeQuery(sqlList);
+	
+		System.out.println("  bill_id"+ "     Name"+"       Surname"+"      status");
+		while (resultSet.next()) {
+			int bill_id = resultSet.getInt("bill_id");
+			String name = resultSet.getString("NAME");
+			String surName = resultSet.getString("SURNAME");
+			String status = resultSet.getString("STATUS");
+
+			System.out.println("    " + resultSet.getInt("bill_id") + "\t     " + resultSet.getString("NAME") + " \t"
+					+ resultSet.getString("SURNAME") + "      \t" + resultSet.getString("STATUS") + "\t ");
+
+		}
+		
+		resultSet.close();
+		
+		
+		
+	}
+	
+	
+	
+	public static void showAllTransactions() throws SQLException {
+		
+		try {
+			Class.forName(DRIVER);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // необязательно
+		
+		Connection con = DriverManager.getConnection(URL, root, password);
+		Statement st = con.createStatement();
+		
+		
+		String sqlList1 = "SELECT * FROM TRANSACTION";
+
+		ResultSet resultSet1 = st.executeQuery(sqlList1);
+
+		
+
+		System.out.println("   ID_Transact"+ "   id"+"      sum\n");
+		while (resultSet1.next()) {
+			int id_transact = resultSet1.getInt("ID_Transact");
+			int id = resultSet1.getInt("id");
+			int sum = resultSet1.getInt("sum");
+			
+			
+			System.out.println("      " + resultSet1.getInt("ID_Transact") + "\t          " + resultSet1.getInt("id") + "\t"
+					+ "  "
+					+ resultSet1.getInt("sum") + "\t ");
+
+		}
+
+		resultSet1.close();
 		
 	}
 
