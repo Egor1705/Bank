fe3be213dcd213b02344ec051194c86d87c881a5
Egor1705 <49376739+Egor1705@users.noreply.github.com>
2019-12-01 16:24:46 +0300

    commit 2

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,3 +1,5 @@
+package main;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -5,31 +7,24 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
+import impl.Transaction;
+
 public class Main {
 
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 
-		Scanner sc = new Scanner(System.in);
-		String user = "USER06A";
-		String password = "USER06A";
-		String databaseURL = "jdbc:db2://172.20.2.116:5035/DALLASB";
-	//	 String databaseURL="jdbc:db2://localhost:5035/DALLASB";
-		Connection conn = null;
-		Statement statement = null;
+Scanner sc = new Scanner(System.in);
+		String driver = "com.mysql.cj.jdbc.Driver";
 		try {
-			Class.forName("com.ibm.db2.jcc.DB2Driver");
-			conn = DriverManager.getConnection(databaseURL, user, password);
-			if (conn != null) {
-				System.out.println("Connected to the database");
-			}
-		} catch (ClassNotFoundException ex) {
-			System.out.println("Could not find DB2 driver class");
-			ex.printStackTrace();
-
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-
-		statement = conn.createStatement();
+		String url = "jdbc:mysql://localhost/Bank?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+		Connection con = DriverManager.getConnection(url, "root", "Bottomass1375");
+		Statement st = con.createStatement();
 
 		boolean check = true;
 		while (check) {
@@ -83,13 +78,14 @@ public class Main {
 				break;
 			}
 			
+			
 			case 6:{
-				Transaction.showAllBills();
+				Transaction.showBills();
 				break;
 			}
 			
 			case 7:{
-				Transaction.showAllTransactions();
+				Transaction.showTransactions();
 				break;
 			}
 
@@ -102,8 +98,95 @@ public class Main {
 
 		}
 		
+//		String sql1 = "CREATE TABLE TRANSACTION(ID_TRANSACT INT NOT NULL,ID INT NOT NULL,SUM INT NOT NULL,"
+//				+ "FOREIGN KEY(ID) references BILL(BILL_ID));";
+//
+//		String sql1 = "CREATE TABLE BILL(BILL_ID INT NOT NULL,NAME VARCHAR(50) NOT NULL,SURNAME VARCHAR(50) NOT NULL,"
+//				+ "STATUS VARCHAR(50) NOT NULL\r\n" + 
+//				",PRIMARY KEY(BILL_ID));                \r\n" + 
+//				"";
+//		
+		
+//		String sqlInsert = "insert into bill values('" +1+ "', '" +"Igor"+  
+//		"', '" +"Sidorov"+ "', '" +"Open"+ "')";
+//				
+//		int x = statement.executeUpdate(sqlInsert);
+//		
+//		if (x > 0) 
+//		{
+//    System.out.println("Successfully Inserted");    
+//		}
+//		else  
+//		{
+//	    System.out.println("Insert Failed");
+//		}
+		
+//statement.executeUpdate(sql1);
+//System.out.println("Database created successfully...");
+		
+//String sql1 = "Drop table Transaction";
+//statement.executeUpdate(sql1);
+//System.out.println("Database was deleted");
+		
+//String sql2 = "Drop table Bill;";
+//statement.executeUpdate(sql2);
+//System.out.println("Database was deleted");
 
-
+//		String sql = "Delete from bill where bill_id=2;";
+//		statement.executeUpdate(sql)
+//		
+		
+//		String sql1 = "delete from bill where id_bill>2;";
+//		st.executeUpdate(sql1);
+//		System.out.println("column was deleted");
+		
+		
+	
+		
+		
+		
+		
+		
+//		String sqlList = "SELECT * FROM BILL";
+//
+//		ResultSet resultSet = st.executeQuery(sqlList);
+//		System.out.println("  id_bill"+ "     Name"+"       Surname"+"      status_of_bill");
+//		while (resultSet.next()) {
+//			int bill_id = resultSet.getInt("id_bill");
+//			String name = resultSet.getString("NAME");
+//			String surName = resultSet.getString("SURNAME");
+//			String status = resultSet.getString("STATUS_of_bill");
+//
+//			System.out.println("    " + resultSet.getInt("id_bill") + "\t     " + resultSet.getString("NAME") + " \t"
+//					+ resultSet.getString("SURNAME") + "      \t" + resultSet.getString("STATUS_of_bill") + "\t ");
+//
+//		}
+//		
+//		resultSet.close();
+//		System.out.println("\n");
+//
+//		String sqlList1 = "SELECT * FROM TRANSACTION";
+//
+//		ResultSet resultSet1 = st.executeQuery(sqlList1);
+//
+//		
+//		System.out.println("   ID"+ "   id_transact_bill"+"   sum\n");
+//		while (resultSet1.next()) {
+//			int id_transact = resultSet1.getInt("ID");
+//			int sum1 = resultSet1.getInt("sum");
+//			int id = resultSet1.getInt("id_transact_bill");
+//			
+//			
+//			System.out.println("   " + resultSet1.getInt("ID") + "\t     " + resultSet1.getInt("id_transact_bill") + "\t"
+//					+ "           "
+//					+ resultSet1.getInt("sum") + "\t ");
+//
+//		}
+//
+//		resultSet1.close();
+//		
+//		
+		
 	}
 
 }
